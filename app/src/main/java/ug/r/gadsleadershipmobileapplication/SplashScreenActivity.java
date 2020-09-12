8ff7878999513fe2bb09c3780a355747943f60ba
william <pandewilliam100@gmail.com>
2020-09-12 12:50:17 +0300

    Initial commit

---
+++ app/src/main/java/ug/r/gadsleadershipmobileapplication/SplashScreenActivity.java
@@ -0,0 +1,14 @@
+package ug.r.gadsleadershipmobileapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
